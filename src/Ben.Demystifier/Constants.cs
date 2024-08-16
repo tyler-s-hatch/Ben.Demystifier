f0456a5cff848638f10370c44af2df2f49a63d50
Tyler Hatch <tyler@tylerhatch.com>
2024-08-16 15:52:31 -0400

    Fix a namespace issue

--- src/Ben.Demystifier/Constants.cs
+++ src/Ben.Demystifier/Constants.cs
@@ -1,7 +1,8 @@
-namespace Ben.Demystifier;
-
-internal static class Constants
+namespace Ben.Demystifier
 {
-    internal const string TrimWarning = "This class should be avoided when compiling for AOT.";
-    internal const string SuppressionResurfaced = "Surfaced by parent class";
+    internal static class Constants
+    {
+        internal const string TrimWarning = "This class should be avoided when compiling for AOT.";
+        internal const string SuppressionResurfaced = "Surfaced by parent class";
+    }
 }
